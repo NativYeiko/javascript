0e20e23dc3b30accf0a9977e396435823f4c26ac
Yeiko <nativ.yeiko@gmail.com>
2018-04-10 15:56:34 +0200

    commit convertisseur monais

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -199,3 +199,73 @@ function newElement() {
     }
   }
 }
+
+//Convertiseur Monnais
+
+var url = 'http://data.fixer.io/api/latest?access_key=eff16e17710c78fc46dd4c53f27e92af&base=EUR&symbols=GBP,JPY,EUR,USD,RUB,CNY,CAD,BTC,BTE';
+
+// define from currency, to currency, and amount
+
+ function recupererValeur() {
+// execute the conversion using the "convert" endpoint:
+$.ajax({
+    url: url,
+    dataType: 'jsonp',
+
+    success: function(res,status,req) {
+
+      console.log(res.rates);
+      console.log(status);
+      var taux = res.rates
+        , from = document.getElementById('from').value
+        , to = document.getElementById('to').value
+        , amount = document.getElementById('fromAmount').value
+
+        var result = amount * taux[to] / taux[from]
+
+
+      document.getElementById("toAmount").value=result
+
+
+    }
+})};
+
+//Compteur de mots
+
+var print = function(msg){
+  document.getElementById("output").innerHTML = "Length is " + msg;
+}
+document.getElementById("btn").onclick = function(event) {
+  print(document.getElementById('str').value.length);
+}
+
+// Conctac Form
+
+
+local = localStorage;
+$(document).ready(function () {
+  function init() {
+    if (localStorage["name"]) {
+      $('#name').val(localStorage["name"]);
+    }
+    if (localStorage["email"]) {
+      $('#email').val(localStorage["email"]);
+    }
+    if (localStorage["selected"]) {
+      $('#selected').val(localStorage["selected"]);
+    }
+    if (localStorage["newletter"]) {
+      $('#newletter').val(localStorage["newletter"]);
+    }
+    if (localStorage["message"]) {
+      $('#message').val(localStorage["message"]);
+    }
+    if (localStorage["submit"]) {
+      $('#submit').val(localStorage["submit"]);
+    }
+  }
+  init();
+});
+$('.stored').change(function () {
+  localStorage[$(this).attr('name')] = $(this).val();
+});
